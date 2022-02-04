bda78c778c098679701959fe7dc051667f43f8e9
rajoy <rajoy@DESKTOP-18KB1V5.lan>
2022-02-04 18:32:30 -0500

    Modified the address tests

--- src/test/java/automation/pages/ShoppingCartSummaryPage.java
+++ src/test/java/automation/pages/ShoppingCartSummaryPage.java
@@ -9,19 +9,48 @@ import org.openqa.selenium.support.How;
 import automation.UI.ActionsCtrlExtn;
 
 public class ShoppingCartSummaryPage extends PageObjectBase {
+	final String expectedAccountName = "David Warner";
+
+	final String emailAddress = "sarabcd@zycd.com";
+	final String gender = "male";
+	final String firstName = "Adam";
+	final String lastname = "levine";
+	final String password = "levine678";
+	final String cityname = "NYC";
+	final String address1 = "5454 70th st newyork";
+	final String postCode = "11379";
+	final String phonenumber = "01711111111";
+	final String day = "8";
+	final String month = "9";
+	final String year = "1992";
+	final String country = "United States";
+	final String state = "New York";
+	final String expected = "MY ACCOUNT";
 
 	@FindBy(how = How.CSS, using = "a[class='button btn btn-default standard-checkout button-medium'] span")
 	private WebElement proceedToCheckOut;
 
+	@FindBy(how = How.CSS, using = ".address.first_item.item.box")
+	private WebElement addressDiv;
+
 	public ShoppingCartSummaryPage(WebDriver driver) {
 		super(driver);
 	}
-	
-	
-	public AddressesPage clickOnCheckout() {
-		
-		new ActionsCtrlExtn(this.proceedToCheckOut,this.driver).clickOnCheckout();
-		return new AddressesPage(this.driver);
+
+	public void clickOnCheckout() {
+		try {
+			if (this.addressDiv.isDisplayed()) {
+				new ActionsCtrlExtn(this.proceedToCheckOut, this.driver).clickOnCheckout();
+
+			}
+		} catch (Exception e) {
+
+			new ActionsCtrlExtn(this.proceedToCheckOut, this.driver).clickOnCheckout();
+			new AddressesPage(this.driver).enterAddress1(address1).enterCityName(cityname).selectState(state)
+					.enterPostCode(postCode).selectCountry(country).enterPhoneNumber(phonenumber).clickregister();
+
+		}
+
 	}
 
 	public LoginPage proceedToCheckout() {
--- src/test/java/automation/tests/AddressTest.java
+++ src/test/java/automation/tests/AddressTest.java
@@ -9,32 +9,14 @@ import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 
 public class AddressTest extends TestBase {
-	final String expectedAccountName = "David Warner";
 	final String email = "abc@abc.com";
 	final String loginpassword = "davidwarner123";
-	final String emailAddress = "sarabcd@zycd.com";
-	final String gender = "male";
-	final String firstName = "Adam";
-	final String lastname = "levine";
-	final String password = "levine678";
-	final String cityname = "NYC";
-	final String address1 = "5454 70th st newyork";
-	final String postCode = "11379";
-	final String phonenumber = "01711111111";
-	final String day = "8";
-	final String month = "9";
-	final String year = "1992";
-	final String country = "United States";
-	final String state = "New York";
-	final String expected = "MY ACCOUNT";
 
 	@Test
 	public void canTestAddress() {
 
 		new LoginPage(this.getDriver()).navigate().login(email, loginpassword).hoverOverProductImage()
-				.clickOnHoverAddToCartButton().clickOnProceedToCheckout().clickOnCheckout().enterAddress1(address1)
-				.enterCityName(cityname).selectState(state).enterPostCode(postCode).selectCountry(country)
-				.enterPhoneNumber(phonenumber).clickregister();
+				.clickOnHoverAddToCartButton().clickOnProceedToCheckout().clickOnCheckout();
 
 		Assert.assertTrue(this.getDriver()
 				.findElement(By.cssSelector("ul[class='address item box'] h3[class='page-subheading']")).isDisplayed());
